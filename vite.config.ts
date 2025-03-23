d9e72f1bb35b07acc2c238898c995ff86c96749a
eskiturk <eskiturk2021@gmail.com>
2025-03-23 23:49:29 +0400

    добавили новый файл пост цсс

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     assetsDir: 'assets',
+    // Отключаем минимизацию для диагностики
+    minify: false,
     // Устанавливаем режим генерации для корректной сборки
     rollupOptions: {
       output: {
